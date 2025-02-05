103427fd4c7f758800596e588adbfb58bade0c0b
Tiago8067 <tiagosoarez6@gmail.com>
2025-02-05 21:16:43 +0000

    modal dos detalhes centrada com bootstrap

--- Tp_DWC/Tp_DWC/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/Tp_DWC.AssemblyInfo.cs
+++ Tp_DWC/Tp_DWC/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/Tp_DWC.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tp_DWC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+48eb6b78400df69a638054450d7e9ebc0914876c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+c2db0b0a47c0baf4bf28c3b233cdc56e9d5564ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tp_DWC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tp_DWC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
